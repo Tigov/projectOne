53a4a9e10ced57f6984b5e0987469f0ca6f04c09
Viktor <vpisarenko23@my.whitworth.edu>
2024-05-01 12:23:36 -0700

    initial commit

---
+++ project-one-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './custom.scss';
+import { Button } from 'react-bootstrap';
+
+function App() {
+
+  return (
+    <div className="App">
+      <button></button>
+    </div>
+  );
+}
+
+export default App;
